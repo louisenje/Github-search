e26d5e6bf70821d7e8ab2f3b6d4eb71c6dc993f3
louisenje <louisenje@gmail.com>
2020-04-14 14:43:23 +0300

    inserted listners and hover

--- src/app/user-det.directive.ts
+++ src/app/user-det.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive , ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appUserDet]'
 })
 export class UserDetDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
-}
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hover('#C46D5E');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hover('#DAA588');
+  }
+
+  private hover(color: string) {
+    this.el.nativeElement.style.borderColor = color;
+  }
+
+
+} 
